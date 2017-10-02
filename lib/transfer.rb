c26ca61888f3a6ec8b75d7de83cae99dc154a766
alpbourne <alpbourne@gmail.com>
2017-10-02 20:26:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,10 +2,10 @@ class Transfer
 
   attr_reader :sender, :receiver
 
-  def initialize(sender, receiver, transfer = 50)
+  def initialize(sender, receiver, status = "pending", transfer = 50)
     @sender = sender
     @receiver = receiver
-    @status = "pending"
+    @status = status
     @transfer = transfer
   end
 end
